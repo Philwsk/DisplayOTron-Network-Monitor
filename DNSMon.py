a82191be3133530cc7b3aecfec4e269d1d28ad20
Philwsk <36168644+Philwsk@users.noreply.github.com>
2018-06-07 21:50:23 +0100

    Added DNSCHECK function
    Added:
    
    A while loop that checks DNSCHECK against my DNS server.
    
    If DNS server is down, LED's go yellow and it says as such.
    
    If device cannot ping google it suggests network is down (this only works if Pi doesn't use the same DNS server).

--- DNSMon.py
+++ DNSMon.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 import time
 import fcntl
@@ -18,6 +19,7 @@ def get_addr(ifname):
         return 'Not Found!'
 
 
+
 wlan0 = get_addr('wlan0')
 eth0 = get_addr('eth0')
 host = socket.gethostname()
@@ -29,35 +31,45 @@ lcd.set_cursor_position(0,0)
 lcd.write('{}'.format(host))
 
 lcd.set_cursor_position(0,1)
-if eth0 != 'Not Found!':
-    lcd.write(eth0)
+if eth0 == 'Not Found!':
+    lcd.write('eth0 DOWN')
 else:
-    lcd.write('eth0 {}'.format(eth0))
+    lcd.write(format(eth0))
 
+nethost = 'www.google.com'
 
-hostname = 'www.google.co.uk'
-response = os.system('ping -c 1 ' + hostname)
-
-lcd.set_cursor_position(0,2)
-while True:
+def DNSCHECK(DNS):
+        response = os.system('ping -c 1 ' + DNS)
+        lcd.set_cursor_position(0,2)
         if response == 0:
                 lcd.write('DNS is UP')
                 backlight.rgb(50,255,50)
                 time.sleep(10)
-                response = os.system('ping -c 1 ' + hostname)
-                lcd.set_cursor_position(0,2)
-        elif response != 0:
-                lcd.write('DNS is DOWN')
-                backlight.rgb(255,50,50)
-                time.sleep(05)
-                response = os.system('ping -c 1 ' + hostname)
-                lcd.set_cursor_position(0,2)
         else:
-                lcd.write('Error')
-                exit(0)
+                netping = os.system('ping -c 1 ' + nethost)
+                if netping == 0:
+                        backlight.rgb(255,255,0)
+                        lcd.write('DNS is DOWN')
+                else:
+                        backlight.rgb(255,0,0)
+                        lcd.write('Network DOWN')
+#       except:
+#               lcd.write('DNS CHECK FAILURE')
+#               backlight.sweep(hue, 0)
 
 
+while True:
+        lcd.clear()
+
+        lcd.set_cursor_position(0,0)
+        lcd.write('{}'.format(host))
+
+        lcd.set_cursor_position(0,1)
+        if eth0 == 'Not Found!':
+                lcd.write('eth0 DOWN')
+        else:
+                lcd.write(format(eth0))
 
 
-
-
+        DNSCHECK('192.168.0.xx')
+        time.sleep(10)
